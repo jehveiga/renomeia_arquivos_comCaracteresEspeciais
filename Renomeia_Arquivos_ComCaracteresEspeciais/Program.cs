991070a3307f3a92cb359db65e685fa32411047e
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-03-10 07:24:30 -0300

    Add project files.

---
+++ Renomeia_Arquivos_ComCaracteresEspeciais/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+
+string pasta = @"C:\Caminho\Para\Sua\Pasta"; // Substitua pelo caminho da sua pasta
+
+if (Directory.Exists(pasta))
+{
+    DirectoryInfo diretorio = new DirectoryInfo(pasta);
+
+    foreach (FileInfo arquivo in diretorio.GetFiles())
+    {
+        string novoNome = RemoverCaracteresEspeciais(arquivo.Name);
+        string novoCaminho = Path.Combine(arquivo.DirectoryName, novoNome);
+
+        if (arquivo.Name != novoNome)
+        {
+            File.Move(arquivo.FullName, novoCaminho);
+            Console.WriteLine($"Renomeado: {arquivo.FullName} para {novoCaminho}");
+        }
+    }
+
+    Console.WriteLine("Concluído!");
+}
+else
+{
+    Console.WriteLine("A pasta especificada não existe.");
+}
+
+
+static string RemoverCaracteresEspeciais(string input)
+{
+    // Remove os caracteres especiais utilizando expressão regular
+    string pattern = "[^a-zA-Z0-9]";
+    string replacement = "_";
+    Regex regex = new Regex(pattern);
+    string novoNome = regex.Replace(input, replacement);
+
+    return novoNome;
+}
+
